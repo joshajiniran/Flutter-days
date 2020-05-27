54f50a007f6efcc4427284cdee36cf86bd025c40
Josh Ajiniran <josuajiniran@gmail.com>
2020-05-27 02:46:47 +0100

    New personal card facelift

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.teal,
+          backgroundColor: Colors.blue,
           body: SafeArea(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 CircleAvatar(
                   backgroundImage: AssetImage('images/josh.png'),
@@ -21,26 +22,105 @@ class MyApp extends StatelessWidget {
                   'Josh Ajiniran',
                   style: TextStyle(
                     fontFamily: 'Pacifico',
-                    fontSize: 28.0,
+                    fontSize: 30.0,
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
                 Text(
-                  'Mobile App Developer',
+                  'Software Engineer',
                   style: TextStyle(
                     letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold,
+                    fontWeight: FontWeight.normal,
                     fontFamily: 'Lobster',
-                    color: Colors.teal[200],
-                  )
+                    fontSize: 16.0,
+                    color: Colors.white,
+                  ),
                 ),
-                Row(
-                  children: <Widget>[
-                    Icon(Icons.phone),
-                    Text('+234 813 6289 02'),
-                  ], 
-                )
+                SizedBox(
+                  height: 20.0,
+                  width: 250.0,
+                  child: Divider(
+                    color: Colors.yellow,
+                    thickness: 2.0,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+                  elevation: 10.0,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+234 813 6286 902',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17.0,
+                      ),
+                    ),
+                    subtitle: Text(
+                      '+234 815 5476 996',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+                  elevation: 5.0,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'josuajiniran@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17.0,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'joshuajiniran@yahoo.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+                  elevation: 10.0,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.store_mall_directory,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'https://github.com/Josh-Ajiniran',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'https://linkedin.com/joshua-ajiniran-72088157',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           )),
